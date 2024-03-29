bae4cd01333674e503b024c8341185afef553dbb
Jedah <davig.silva1527@gmail.com>
2024-03-29 11:02:59 -0300

    add jogo da advinhação
    Adicionado o jogo da advinhação no menu.

--- exercicios arg/menu.py
+++ exercicios arg/menu.py
@@ -14,6 +14,27 @@ def parimpar():
     else: #PAR COM 1
         print(f'Você perdeu, você escolheu {jogador} e o número {num}, o computador escolheu o número {comp}. A soma dos dois números é {soma}.')
     return
+def advsif():#opção do jogo da advinhação sem o if
+    import random
+    jogador = int(input('escolha um número de 1 a 10 que você acha que a máquina vai jogar: '))
+    maquina = random.choice ([0, 1, 2, 3, 4, 5, 6])
+    resultado = (maquina == jogador)
+    print(resultado)
+    return
+def advcif():#opção do jogo da advinhação com o if
+    import random
+    jogador = int(input('Advinhe o número que a máquina vai jogar: (de 1 a 10)'))
+    maquina = random.choice([0, 1, 2, 3, 4, 5, 6])
+    if(jogador == maquina):
+        print(f'você venceu, a maquina jogou {maquina} e você jogou {jogador}.')
+    else:
+        print(f'Você perdeu, a máquina jogou {maquina} e você jogou {jogador}.')
+    return
+
+
+
+
+
 
 
 
